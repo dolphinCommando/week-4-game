cefdf4497a795b1ef013298e29da48a09497b5e4
Matthew Buchanan <matt@matthews-air.home>
2018-03-18 16:17:46 -0400

    mission accomplished screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,14 +8,12 @@ $(document).ready(function() {
     var levelUpIncrement;
     var battleNotOver;
     var enemiesDefeated;
-    //var heroCSS = {hover: {}};
-    //var villainCSS = {hover: {}};
-    //console.log($('.villains'));
-    //console.log($('.villains')[2]);
-    var villainObject = [];
+    var numberOfEnemies;
 
     $('#intro').on('click', '.heroes', function(event) {
         setVariables();
+        numberOfEnemies = $('#arena .villains').length;
+        console.log(numberOfEnemies);
         jediSkill = +$(this).attr('data-skill');
         jediStamina = +$(this).attr('data-stamina');
         isVader = $(this).attr('data-vader');
@@ -25,6 +23,7 @@ $(document).ready(function() {
         $('#arena').css('visibility', 'visible');
         //saveCSS(this, 'good');
         deactivateLink(copy);
+        $('#gamewon').append(copy.clone());
         $('#imagestore').empty();
         
     });
@@ -40,7 +39,7 @@ $(document).ready(function() {
         $('#battle').css('visibility', 'visible');
         $('#battle h2').text('Press attack buttom below to simulate an epic Jedi battle.');
         $('#next').css('visibility', 'hidden');
-        $('#new').css('visibility', 'hidden');
+        $('.new').css('visibility', 'hidden');
         //saveCSS(this, 'evil');
         deactivateLink(copy);
     });
@@ -53,6 +52,7 @@ $(document).ready(function() {
        if (sithStamina <= 0) {
             jediSkill += 2 * levelUpIncrement;
             enemiesDefeated++;
+            numberOfEnemies--;
             $('#battle h2').append('<p>Enemy defeated!</p>');
             $('#next').css('visibility', 'visible');
             battleNotOver = false;
@@ -61,7 +61,7 @@ $(document).ready(function() {
            $('#battle h2').append('<p>You have been slain!</p>');
            $('#battle h2').append('<p>Jedi skill learned: ' + jediSkill + '</p>');
            $('#battle h2').append('<p>Enemies defeated: ' + enemiesDefeated + '</p>');
-           $('#new').css('visibility', 'visible');
+           $('#battle .new').css('visibility', 'visible');
            battleNotOver = false;
        }
        $('#stats').append('<p>Skill: ' + jediSkill + ' Stamina: ' + jediStamina + ' Enemy Stamina ' + sithStamina + '</p>');
@@ -80,12 +80,20 @@ $(document).ready(function() {
         $('#alive').remove('#battle .villains');
         $('#battle').remove('.villains');
         $('#battle').css('visibility', 'hidden');
-        $('#arena').css('visibility', 'visible');
         $('#next').css('visibility', 'hidden');
-        battleNotOver = true;
+        if (numberOfEnemies === 0) {
+            $('#gamewon').css('visibility', 'visible');
+            $('#gamewon .new').css('visibility', 'visible');
+            $('#gamewon .score').html('<p>Enemies defeated: ' + enemiesDefeated + '</p>' + '<p>Final skill level: ' + jediSkill + '</p>' + '<p>Stamina remaining: ' + jediStamina + '</p>');
+        }
+        else {
+            $('#arena').css('visibility', 'visible');
+            battleNotOver = true;
+        }
+        
     });
 
-    $('#new').click(function() {
+    $('.new').click(function() {
         console.log('New game started');
         //defaultHeroCSS('.heroes');
         //defaultHeroCSS('.villains');
@@ -99,46 +107,18 @@ $(document).ready(function() {
         $('#arena').css('visibility', 'hidden');
         $('#intro').css('visibility', 'visible');
         $('#new').css('visibility', 'hidden');
+        $('#gamewon .heroes').remove();
+        $('#gamewon').css('visibility', 'hidden');
+        $('.new').css('visibility', 'hidden');
 
-        //Changing the CSS needs work!
     });
-/*
-    function saveCSS(selector, allegiance) {
-        if (allegiance === 'good') {
-            heroCSS.cursor = $(selector).css('cursor');
-            heroCSS.background = $(selector).css('background-color');
-            heroCSS.opacity = $(selector).css('opacity');
-            heroCSS.hover.cursor = $(selector + ':hover').css('cursor');
-            heroCSS.hover.background = $(selector + ':hover').css('background-color');
-            heroCSS.hover.opacity = $(selector + ':hover').css('opacity');  
-        }
-        else if (allegiance === 'evil') {
-            villainCSS.cursor = $(selector).css('cursor');
-            villainCSS.background = $(selector).css('background-color');
-            villainCSS.opacity = $(selector).css('opacity');
-            villainCSS.hover.cursor = $(selector + ':hover').css('cursor');
-            villainCSS.hover.background = $(selector + ':hover').css('background-color');
-            villainCSS.hover.opacity = $(selector + ':hover').css('opacity');
-        }
-    }
-*/
+
     function deactivateLink(selector) {
         $(selector).css('cursor', 'default');
         $(selector).css('background', 'white');
         $(selector).css('opacity', '1.0');
     }
- /*   
-    function defaultCSS(selectors) {
-        for (let item in selectors) {
-            console.log($(selectors[item]));
-            $(selectors[item]).css('cursor', heroCSS.cursor);
-            $(selectors[item]).css('background', heroCSS.background);
-            $(selectors[item]).css('opacity', heroCSS.opacity);
-        }
-        
-    }
-*/
-
+ 
     function setVariables() {
         jediSkill = 0;
         jediStamina = 0;
@@ -150,5 +130,6 @@ $(document).ready(function() {
         enemiesDefeated = 0;
     }
 
+    
 
 });
