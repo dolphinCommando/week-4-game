981898b4162f6a1474ddefbbf5f6c93e6537538d
Matthew Buchanan <matt@matthews-air.home>
2018-03-18 00:01:53 -0400

    basic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,16 @@
 
 $(document).ready(function() { 
-    var heroSkill = 0;
-    var heroStamina = 0;
+    var jediSkill = 0;
+    var jediStamina = 0;
     var isVader = false;
+    var sithSkill = 0;
+    var sithStamina = 0;
+    var levelUpIncrement = 5;
+    var battleNotOver = true;
     
     $('#intro').on('click', '.heroes', function(event) {
-        heroSkill = $(this).attr('data-skill');
-        heroStamina = $(this).attr('data-stamina');
+        jediSkill = +$(this).attr('data-skill');
+        jediStamina = +$(this).attr('data-stamina');
         isVader = $(this).attr('data-vader');
         $('#battle').append($(this));
         $('#intro').css('visibility', 'hidden');
@@ -17,18 +21,62 @@ $(document).ready(function() {
 
     
     $('#alive').on('click', '.villains', function(event) {
+        sithSkill = +$(this).attr('data-skill');
+        sithStamina = +$(this).attr('data-stamina');
         $('#battle').append($(this));
         $('#arena').css('visibility', 'hidden');
         $('#battle').css('visibility', 'visible');
+        $('#battle h2').text('Press attack buttom below to simulate an epic Jedi battle.');
+        $('#next').css('visibility', 'hidden');
         deactivateLink(this);
     });
 
+    $('#battle').on('click', '#attack', function(event) {
+    if (battleNotOver) {
+       sithStamina -= jediSkill;
+       if (sithStamina <= 0) {
+            jediSkill += 2 * levelUpIncrement;
+            $('#battle h2').prepend('<p>Enemy defeated!</p>');
+            $('#next').css('visibility', 'visible');
+            battleNotOver = false;
+       }
+       else if (jediStamina <= 0) {
+           $('#battle h2').prepend('<p>You have been slain!</p>');
+           // needs lose condition 
+       }
+       else {
+           jediStamina -= sithSkill;
+           jediSkill += levelUpIncrement;
+       }
+       $('#stats').append('<p>Skill: ' + jediSkill + ' Stamina: ' + jediStamina + ' Enemy Stamina ' + sithStamina + '</p>');
+    }
+       
+    });
+
+    
+
+    $('#next').click(function() {
+        console.log($('#battle > .villains'));
+        $('#stats').empty();
+        $('#battle h2').empty();
+        $('#battle .villains').css('opacity', '0.5'); 
+        $('#defeated').append($('#battle > .villains'));
+        $('#arena').remove('#battle > .villains');
+        $('#battle').remove('.villains');
+        $('#battle').css('visibility', 'hidden');
+        $('#arena').css('visibility', 'visible');
+        $('#next').css('visibility', 'hidden');
+        battleNotOver = true;
+    });
+
+    
 
     function deactivateLink(selector) { 
         $(selector).css('cursor', 'default');
         $(selector).css('background', 'white');
         $(selector).css('opacity', '1.0');
     }
+    
     
 
 
